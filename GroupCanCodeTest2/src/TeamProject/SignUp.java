eb322c486de76ac1ef2358851c2b3ea10bba44c0
SantiagoAB2001 <91096467+SantiagoAB2001@users.noreply.github.com>
2021-11-03 16:49:12 -0700

    Added SignUp Class!

---
+++ GroupCanCodeTest2/src/TeamProject/SignUp.java
@@ -0,0 +1,13 @@
+package TeamProject;
+
+public class SignUp {
+/*
+	Class: SignUp        object: User(string create_user, string create_password)
+	Verify age >= 13
+	Verify email address
+	(Optional) Phone Number
+	Grab username
+	Grab password
+*/
+
+}

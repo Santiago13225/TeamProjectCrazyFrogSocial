59a25705caedeb6f3b8ee2be0926fe3a24bc99b1
SantiagoAB2001 <91096467+SantiagoAB2001@users.noreply.github.com>
2021-11-03 16:01:57 -0700

    Added my name!

---
+++ GroupCanCodeTest2/src/TeamProject/TeamProjectTest2.java
@@ -0,0 +1,21 @@
+package TeamProject;
+
+public class TeamProjectTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("SacStateID - This line was added by Santiago Bermudez.");
+		System.out.println("SacStateID - This line was added by First name Last	name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last	name.");
+	}
+		
+}

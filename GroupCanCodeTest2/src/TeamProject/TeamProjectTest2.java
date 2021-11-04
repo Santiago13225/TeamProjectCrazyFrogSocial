0f64e25000ecd33f8086b70133f4caa9cf0d29d4
Dane Coleman <danecoleman@csus.edu>
2021-11-03 18:26:16 -0700

    Dane added name

--- GroupCanCodeTest2/src/TeamProject/TeamProjectTest2.java
+++ GroupCanCodeTest2/src/TeamProject/TeamProjectTest2.java
@@ -8,7 +8,7 @@ public class TeamProjectTest2 {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by Santiago Bermudez.");
-		System.out.println("SacStateID - This line was added by First name Last	name.");
+		System.out.println("301719561  - This line was added by Dane Coleman.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");

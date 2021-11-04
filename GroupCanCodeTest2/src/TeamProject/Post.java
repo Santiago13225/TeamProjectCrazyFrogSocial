87dc8b0784052ace6a26423e1accb994b6534fb1
Eric Truong <truong.eric916@gmail.com>
2021-11-03 19:49:32 -0700

    Post.java
    Added a post class with delete method

---
+++ GroupCanCodeTest2/src/TeamProject/Post.java
@@ -0,0 +1,14 @@
+class Post {
+    private String postDescription;
+    //Temp placeholder for img
+    private int picture;
+    public Post(String postDescription, int picture){
+        this.postDescription = postDescription;
+        this.picture = picture;
+    }
+
+    //Delete Post Method
+    public void deletePost(Post post){
+        //We would remove the post from the database and also remove it from our profile.
+    }
+}

83e049fa27247a0bd32bdbc02cf7b937909602a7
TylerIto <91100218+TylerIto@users.noreply.github.com>
2021-11-09 10:22:26 -0800

    added name

--- GroupCanCodeTest2/src/TeamProject/TeamProjectTest2.java
+++ GroupCanCodeTest2/src/TeamProject/TeamProjectTest2.java
@@ -11,7 +11,7 @@ public class TeamProjectTest2 {
 		System.out.println("301719561  - This line was added by Dane Coleman.");
 		System.out.println("219318451 - This line was added by Eric Truong.");
 		System.out.println("301455713 - This line was added by Matthew Petruescu.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by Tyler Ito.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");

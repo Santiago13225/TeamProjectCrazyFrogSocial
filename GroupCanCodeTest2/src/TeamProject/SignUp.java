957c11575931e487c640b812a8ee350bcce9514f
Christian Long <chrislong@csus.edu>
2021-11-09 12:19:51 -0800

    updated Signup

--- GroupCanCodeTest2/src/TeamProject/SignUp.java
+++ GroupCanCodeTest2/src/TeamProject/SignUp.java
@@ -1,13 +1,51 @@
 package TeamProject;
 
-public class SignUp {
-/*
-	Class: SignUp        object: User(string create_user, string create_password)
-	Verify age >= 13
-	Verify email address
-	(Optional) Phone Number
-	Grab username
-	Grab password
-*/
+public class SignUp extends User{
 
+	private String birthday;
+
+	public SignUp()
+    {
+        // call the superclass default constructor to
+        // initialize username,password,emailaddress,phonenumber.
+        super();
+        // update values for email, username, password, phone number
+        verifyEmailAddress();
+        grabUsername();
+        grabPassword();
+        grabPhoneNumber();
+        // initialize birthday and enter birthday.
+        verifyAge();
+        
+    }
+
+	public static void CreateAccount() {
+		// get user input for username,password,birthday,email,phone
+		Signup newAccount = new SignUp();
+		// verify age is over 13
+		// verify email is an email address
+		// verify username is entered
+		// verify password is entered
+		// if all are verified then proceed with creating a new account by 
+		// saving newAccount to database
+	}
+	
+	public void verifyAge(birthday) {
+		this.birthday = birthday;
+	}
+	public void verifyEmailAddress(email) {
+		this.birthday = birthday;
+	}
+
+	public void grabUsername(username) {
+		this.username = username;
+	}
+	
+	public void grabPassword(password) {
+		this.password = password;
+	}
+	public void grabPhoneNumber(phonenumber) {
+		this.phone = phonenumber;
+	}
+	
 }

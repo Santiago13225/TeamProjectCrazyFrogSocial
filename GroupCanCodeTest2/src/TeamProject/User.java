ed0a788dca93fce94f682f5655d23fde9921fdfd
Dane Coleman <danecoleman@csus.edu>
2021-11-11 00:36:27 -0800

    Dane updated User class

--- GroupCanCodeTest2/src/TeamProject/User.java
+++ GroupCanCodeTest2/src/TeamProject/User.java
@@ -1,11 +1,24 @@
 package TeamProject;
 
-public class User {
-	/*
-	 user ID
-	 UserName
-	 Password
-	 Email Address
-	 Phone Number
-	*/
+public class User{
+    private int userID;
+    private String userName;
+    private String password;
+    private String email;
+    private String phoneNumber; //string best to include dashes/spaces if user enters that
+    private boolean Premium;
+    private boolean showAds = true;
+
+    User(String UserName, String password, String email, String phoneNumber){
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+    public static void showAds(public Premium){
+        if(!Premium){
+            showAds = false;
+        }
+    }
+
 }

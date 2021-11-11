b5f9699ded300fb13fad9ec936c4719fd47cd5e1
Dane Coleman <danecoleman@csus.edu>
2021-11-11 00:37:58 -0800

    added PremiumStatus class

---
+++ GroupCanCodeTest2/src/TeamProject/PremiumStatus.java
@@ -0,0 +1,11 @@
+class PremiumStatus{
+    private boolean Premium;
+
+    PremiumStatus(){
+        Premium = true;
+    }
+    public static void Advertising(){
+        User.showAds(Premium);
+    }
+
+}

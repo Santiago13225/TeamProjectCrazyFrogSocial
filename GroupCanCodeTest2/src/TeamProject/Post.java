1357b26671a41b811184d7148d21eb9c65283ece
Eric Truong <truong.eric916@gmail.com>
2021-11-11 12:28:51 -0800

    Update Post.java

--- GroupCanCodeTest2/src/TeamProject/Post.java
+++ GroupCanCodeTest2/src/TeamProject/Post.java
@@ -1,15 +1,26 @@
 package TeamProject;
 class Post {
+    private int postID;
     private String postDescription;
     //Temp placeholder for img
     private int picture;
-    public Post(String postDescription, int picture){
+    public Post(int postID, String postDescription, int picture){
+        this.postID = postID;
         this.postDescription = postDescription;
         this.picture = picture;
     }
 
     //Delete Post Method
-    public void deletePost(Post post){
-        //We would remove the post from the database and also remove it from our profile.
+    public void deletePost(int postID, String desciption, int picture){
+        if(postID == postID){
+    //remove postID
+}
     }
+
+    public void searchProfile(int postID){
+        //Scan for user input
+        //Check if user exists in the database using an if statement
+        //Show the profile if it exists, if it does not exists show nothing or an error.
+    }
+
 }

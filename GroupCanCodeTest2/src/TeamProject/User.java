5f769b285150dac5d39a520598ab99a4d6512f94
daneski9 <91101595+daneski9@users.noreply.github.com>
2021-11-11 14:07:04 -0800

    Update User.java

--- GroupCanCodeTest2/src/TeamProject/User.java
+++ GroupCanCodeTest2/src/TeamProject/User.java
@@ -7,13 +7,14 @@ public class User{
     private String email;
     private String phoneNumber; //string best to include dashes/spaces if user enters that
     private boolean Premium;
-    private boolean showAds = true;
+    private boolean showAds;
 
     User(String UserName, String password, String email, String phoneNumber){
         this.userName = userName;
         this.password = password;
         this.email = email;
         this.phoneNumber = phoneNumber;
+        showAds = true;
     }
     public static void showAds(public Premium){
         if(Premium){

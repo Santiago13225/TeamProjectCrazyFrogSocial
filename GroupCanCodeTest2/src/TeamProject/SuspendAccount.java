0135ddaa0919a73b806386364cdb99a6d43c5dca
SantiagoAB2001 <91096467+SantiagoAB2001@users.noreply.github.com>
2021-11-11 10:35:13 -0800

    Added Admin Classes!

---
+++ GroupCanCodeTest2/src/TeamProject/SuspendAccount.java
@@ -0,0 +1,14 @@
+package TeamProject;
+
+public class SuspendAccount {
+	
+	public static void suspendAccount() {
+		System.out.println("Account suspended!");
+	}
+	
+	public static void removeSuspension() {
+		System.out.println("Suspension removed!");
+	}
+	
+	
+}

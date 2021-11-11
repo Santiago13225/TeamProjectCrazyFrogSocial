cbb376a0d00ac33cb68dc1d895a30a01e7c5371f
TylerIto <91100218+TylerIto@users.noreply.github.com>
2021-11-11 13:04:20 -0800

    Create VideoPlayer.java

---
+++ VideoPlayer.java
@@ -0,0 +1,14 @@
+package TeamProject;
+
+public class VideoShort{
+  private String video;
+  private int volume;
+  
+  public void play(){
+  }
+  public void pause(){
+  }
+  public int setVolume(int volume){
+    this.volume = volume;
+  }
+}
